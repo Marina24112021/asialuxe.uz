5d09167c28d1d8d2ea1980ed45f99bc23c49a4b8
Marina Chen <chen.marina.vm10@gmail.com>
2025-01-12 23:56:27 +0500

    diploma

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -2,7 +2,6 @@ package helpers;
 
 import java.util.List;
 
-import static drivers.BrowserstackDriver.config;
 import static helpers.ReadFileToList.readSecretFiles;
 import static io.restassured.RestAssured.given;
 import static specs.CodeAsiaLuxeSpec.successfulResponse200Spec;
