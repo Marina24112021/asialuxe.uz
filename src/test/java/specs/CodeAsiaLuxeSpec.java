ca75e436a7d60e50776cb95710df4abdff98dab7
Marina Chen <chen.marina.vm10@gmail.com>
2025-01-17 15:43:16 +0500

    changes

--- src/test/java/specs/CodeAsiaLuxeSpec.java
+++ src/test/java/specs/CodeAsiaLuxeSpec.java
@@ -4,10 +4,15 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import static helpers.CustomAllureListener.withCustomTemplates;
+import static helpers.ReadFileToList.readSecretFiles;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.http.ContentType.JSON;
+import static specs.Endpoints.SECRET_FILE_PATH_FOR_AUTHORIZATION;
 
 public class CodeAsiaLuxeSpec {
     public final static ResponseSpecification successfulResponse200Spec = new ResponseSpecBuilder()
@@ -18,27 +23,26 @@ public class CodeAsiaLuxeSpec {
             .expectStatusCode(404)
             .log(ALL)
             .build();
-    // static Properties secretToken;
+     static Properties secretToken;
     public final static RequestSpecification requestSpecForAuth = with()
             .baseUri("https://api.asialuxe.app")
-            .header("Authorization", "Bearer oqO7ALyHbi41BKJ-7QJ7PXLa59P1LEGmQp0UlkWCunsqqsQvz_1a5jFKRvHM_anc")
-            // .header("Authorization", secretToken.getProperty("pathauth"))
+            .header("Authorization", secretToken.getProperty("pathauth"))
             .filter(withCustomTemplates())
             .contentType(JSON)
             .log().all();
     public final static RequestSpecification requestSpec = with()
             .baseUri("https://api.asialuxe.app")
             .header("Authorization", "")
-            // .header("Authorization", secretToken.getProperty("path"))
+            .header("Authorization", secretToken.getProperty("path"))
             .filter(withCustomTemplates())
             .contentType(JSON)
             .log().all();
 
-   /* static {
+    static {
         try {
             secretToken = readSecretFiles(SECRET_FILE_PATH_FOR_AUTHORIZATION);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }*/
+    }
 }
