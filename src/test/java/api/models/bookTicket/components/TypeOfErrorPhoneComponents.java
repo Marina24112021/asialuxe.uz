fe7b8294af5e0426db28981e8d90f40c6818ca5d
Marina Chen <chen.marina.vm10@gmail.com>
2025-01-19 03:15:12 +0500

    changes

---
+++ src/test/java/api/models/bookTicket/components/TypeOfErrorPhoneComponents.java
@@ -0,0 +1,12 @@
+package api.models.bookTicket.components;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class TypeOfErrorPhoneComponents {
+    List<String> phone;
+}
